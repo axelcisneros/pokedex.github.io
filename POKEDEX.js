9d058c9485ac897cc55d6f24639e095c39b1f34c
axelcisneros <99311637+axelcisneros@users.noreply.github.com>
2022-03-20 19:02:49 -0600

    fix:code

--- POKEDEX.js
+++ POKEDEX.js
@@ -7,16 +7,33 @@ const fetchPokemon = () => {
         if (res.status != "200") {
             console.log(res);
             pokeImage("./img/sad.png")
+            pokeImage2("./img/sad.gif")
+            tipo("N")
+            tipo2("N")
         }
         else {
             return res.json();
         }
     }).then((data) => {
         if (data) {
-            console.log(data);
+            console.log(data);            
             let pokeImg = data.sprites.front_default;
             pokeImage(pokeImg);
             console.log(pokeImg);
+            let pokeImg2 = data.sprites.front_shiny;
+            pokeImage2(pokeImg2);
+            console.log(pokeImg2);
+            let poketyp = data.types[0].type.name;
+            tipo(poketyp)
+            console.log(poketyp);
+            if (data.types.length == 2) {
+                console.log(data.types)
+                let poketyp2 = data.types[1].type.name;
+                tipo2(poketyp2)
+                console.log(poketyp2);
+            }else{
+                tipo2("N/A");
+            }            
         }
     });
 }
@@ -24,4 +41,17 @@ const fetchPokemon = () => {
 const pokeImage = (url) => {
     const pokePhoto = document.getElementById("pokeImg");
     pokePhoto.src = url;
+}
+
+const pokeImage2 = (url) => {
+    const pokePhoto = document.getElementById("pokeImg2");
+    pokePhoto.src = url;
+}
+const tipo = (url) => {
+    const poketype = document.getElementById("typ");
+    poketype.value = url;
+}
+const tipo2 = (url) => {
+    const poketyp2 = document.getElementById("typ2");
+    poketyp2.value = url;
 }
